e8b16ecd1b148d9f0db2948174774124ce090f84
Jong Wook Kim <jongwook@nyu.edu>
2018-03-31 17:43:51 -0400

    added usual setup metadata

--- setup.py
+++ setup.py
@@ -3,6 +3,26 @@ from Cython.Build import cythonize
 
 setup(
     name = 'tfrecord_lite',
+    version = '0.0.1',
+    description = 'A lightweight tfrecord parser',
+    url = 'https://github.com/jongwook/tfrecord_lite',
+    author = 'Jong Wook Kim',
+    author_mail = 'jongwook@nyu.edu',
+    license = 'MIT',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords = 'tfrecord',
+    project_urls = {
+        'Source': 'https://github.com/jongwook/tfrecord_lite',
+        'Tracker': 'https://github.com/jongwook/tfrecord_lite/issues'
+    },
+    install_requires = ['numpy'],
+    python_requires = '>=3.6',
     ext_modules = cythonize(Extension(
         'tfrecord_lite',
         sources=[
